2678a1d951cb706639c1d0b117ae7c3f6344ab17
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2019-05-11 14:29:51 +0530

    Restricted countries to resolve data issue for Cities

--- src/geographic.ts
+++ src/geographic.ts
@@ -1,6 +1,6 @@
 import { randomItem } from '@v8187/rs-utils';
 
-import countriesData from './static/countries.data';
+import countriesData from './static/countries5.data';
 import statesData from './static/states.data';
 import citiesData from './static/cities.data';
 
@@ -45,7 +45,7 @@ export const country = (): string => {
 };
 
 /**
- * Generates random State/Provision name based on given Countries list
+ * Generates random State/Province name based on given Countries list
  * 
  * @param options { ICountryBasedOptions }
  * @returns { string }
@@ -63,12 +63,48 @@ export const state = (options: ICountryBasedOptions = {}): string => {
  * @returns { string }
  */
 export const city = (options: ICityOptions = {}): string => {
-    const countries = countryNames(options);
-    const states = options && options.states ? options.states.map(state => state.id) : [];
-    const filtered = (countries.length || states.length) ?
-        citiesData.filter(cityItem => {
-            return countries.indexOf(cityItem.country) !== -1 && states.indexOf(cityItem.state) !== -1;
-        }) : citiesData;
+    const optCountries = countryNames(options);
+    const optStates = options && options.states ? options.states.map(state => state.id) : [];
+    // const filtered = (optCountries.length || optStates.length) ?
+    //     citiesData.filter(cityItem => {
+    //         return optCountries.indexOf(cityItem.country) !== -1 && optStates.indexOf(cityItem.state) !== -1;
+    //     }) : citiesData;
+
+    let filtered: any[] = [];
+    // If countries available
+    if (optCountries.length) {
+        /**
+         * If States avialable, filter out states for given countries only
+         */
+        let filteredStates: string[] = [];
+
+        if (optStates.length) {
+            statesData.map(stateItem => {
+                if (optCountries.indexOf(stateItem.country) !== -1 && optStates.indexOf(stateItem.id) !== -1) {
+                    filteredStates.push(stateItem.id);
+                }
+            });
+
+            if (filteredStates.length) {
+                filtered = citiesData.filter(cityItem => filteredStates.indexOf(cityItem.state) !== -1);
+            }
+        }
+        /**
+         * If States not available in options OR
+         * not found for given countries
+         */
+        if (!filteredStates.length) {
+            filtered = citiesData.filter(cityItem => optCountries.indexOf(cityItem.country) !== -1);
+        }
+    }
+    // If ONLY states available
+    else if (!optCountries.length && optStates.length) {
+        filtered = citiesData.filter(cityItem => optStates.indexOf(cityItem.state) !== -1);
+    }
+    // If both are not avialable
+    else if (!optCountries.length && !optStates.length) {
+        filtered = citiesData;
+    }
 
     return randomItem(filtered).name;
 };
--- src/static/countries5.data.ts
+++ src/static/countries5.data.ts
@@ -1,14 +1,14 @@
 export default [
-    { name: 'Australia', isdCode: '61', code2: 'AU', code3: 'AUS' },
+    // { name: 'Australia', isdCode: '61', code2: 'AU', code3: 'AUS' },
     { name: 'Brazil', isdCode: '55', code2: 'BR', code3: 'BRA' },
-    { name: 'Canada', isdCode: '1', code2: 'CA', code3: 'CAN' },
+    // { name: 'Canada', isdCode: '1', code2: 'CA', code3: 'CAN' },
     { name: 'China', isdCode: '86', code2: 'CN', code3: 'CHN' },
     { name: 'France', isdCode: '33', code2: 'FR', code3: 'FRA' },
     { name: 'India', isdCode: '91', code2: 'IN', code3: 'IND' },
-    { name: 'Myanmar', isdCode: '95', code2: 'MM', code3: 'MMR' },
-    { name: 'Singapore', isdCode: '65', code2: 'SG', code3: 'SGP' },
-    { name: 'Spain', isdCode: '34', code2: 'ES', code3: 'ESP' },
-    { name: 'United Arab Emirates', isdCode: '971', code2: 'AE', code3: 'ARE' },
-    { name: 'United Kingdom', isdCode: '44', code2: 'GB', code3: 'GBR' },
+    // { name: 'Myanmar', isdCode: '95', code2: 'MM', code3: 'MMR' },
+    // { name: 'Singapore', isdCode: '65', code2: 'SG', code3: 'SGP' },
+    // { name: 'Spain', isdCode: '34', code2: 'ES', code3: 'ESP' },
+    // { name: 'United Arab Emirates', isdCode: '971', code2: 'AE', code3: 'ARE' },
+    // { name: 'United Kingdom', isdCode: '44', code2: 'GB', code3: 'GBR' },
     { name: 'United States', isdCode: '1', code2: 'US', code3: 'USA' }
 ];
